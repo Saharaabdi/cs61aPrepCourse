36e6a9be3383630b5fb3b5ef6784e4853a7d3119
Sahara Abdi <saharaabdi@airbears2-10-142-147-92.airbears2.1918.berkeley.edu>
2016-12-31 15:26:29 -0800

    updates

---
+++ Chapter 6/Angry boss.rb
@@ -0,0 +1,3 @@
+puts 'what do you want? '
+want= gets.chomp
+puts ('whaddaya mean' + want +'?!? '+ 'you are fired' + '! !').upcase
---
+++ Chapter 6/Table of content.rb
@@ -0,0 +1,9 @@
+str1= 'Table of Content'.center(40)
+str2 = 'Chapter 1: Getting Started'.ljust(20) + 'page 1'.rjust(15)
+str3 = 'Chapter 2: Numbers'.ljust(20) +         'page 9'.rjust(20)
+str4 = 'chapter 3: Letters'.ljust(20) +         'page 13'.rjust(20)
+puts str1
+puts 
+puts str2
+puts str3
+puts str4
